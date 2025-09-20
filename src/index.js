a050401145a70483c8478d963704c1a00ab53860
nguyenphong1996 <phongndbps35129@fpt.edu.vn>
2025-09-21 02:05:22 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const apiRouter = require('./routes/index');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    app.use('/api', apiRouter);
+
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+run();
