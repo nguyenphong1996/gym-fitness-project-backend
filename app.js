4c18ad3eb7a77240d71a44530991d21e6f927bb2
nguyenphong1996 <phongndbps35129@fpt.edu.vn>
2025-10-09 02:35:40 +0700

    Connected MongoDB Atlas successfully

---
+++ app.js
@@ -0,0 +1,27 @@
+
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const connectDB = require('./src/config/db');
+require('dotenv').config();
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+connectDB();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.get('/', (req, res) => res.send('Server OK + MongoDB connected!'));
+
+module.exports = app;
+
+
